4f8cc9959a14b347706e7f59061cf9cacaa878c7
Gisele Tonello Bringhenti <giseletbrin@gmail.com>
2023-03-01 19:13:15 -0300

    fix: email

--- email.js
+++ email.js
@@ -2,7 +2,7 @@ function SendEmail() {
   var params = {
     from_name : document.getElementById("name").value,
     email_id : document.getElementById("email").value,
-    menssage : document.getElementById("message").value
+    message : document.getElementById("message").value
   }
 
   emailjs.send("service_dcq6vlk", "template_u1c3bhn", params).then(function (res) {
