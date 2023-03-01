5c93256e1cf2578e7ec1f00f44b4d98662737dde
Gisele Tonello Bringhenti <giseletbrin@gmail.com>
2023-03-01 17:58:42 -0300

    fix: deleted preloader

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// PreLoader
-jQuery.noConflict();
-(function($) {
-	$(window).on('load', function() { // makes sure the whole site is loaded
-		$('#status').fadeOut(); // will first fade out the loading animation
-		$('#preloader').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	});
-})(jQuery);
-
 // Scroll to Top
 jQuery.noConflict();
 (function($) {
